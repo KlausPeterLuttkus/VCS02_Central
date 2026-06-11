0195f417470ca9102be8a0f9334eaa0c246475d5
Klaus-Peter Luttkus <klaus.peter@luttkus-bremen.de>
2026-06-11 16:03:16 +0200

    Final

--- Programs/Rules.mk
+++ Programs/Rules.mk
@@ -4,8 +4,8 @@ HELLOWORLD.PGM: helloworld.pgm.rpgle
 # Standalone Programm mit Bildschirmdialog
 HELLODSPF.PGM: hellodspf.pgm.rpgle hellodspf.dspf
 
-# Modul erstellen für HELLOMOS.PGM
-HELLOMOD.MODULE: hellomod.pgm.rpgle
+# Modul erstellen für HELLOMOD.PGM
+HELLOMOD.MODULE: hellomod.rpgle
 # Programm HELLOMOD.PGM binden mit Modul HELLOMOD.MODULE
 HELLOMOD.PGM: HELLOMOD.MODULE
 
